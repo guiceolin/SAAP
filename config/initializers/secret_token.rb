d8f7546d8a63c169ebff27be741c5b32e2a37dd1
Guilherme Ceolin <guiceolin@gmail.com>
2013-08-21 20:06:19 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SAAP::Application.config.secret_key_base = '207939eb2588fa3f2674297b54ecdb7f656349d6fc2607736dc49372641120f0e11a8dc8408ac355ac6924532592ead81dbcc10782436ade394742514b26ef4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SAAP::Application.config.session_store :cookie_store, key: '_SAAP_session'
