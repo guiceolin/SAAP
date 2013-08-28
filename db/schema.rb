202a86242c44097979c95d93ef017858a093e5e1
Guilherme Ceolin <guiceolin@gmail.com>
2013-08-28 19:37:17 -0300

    Adicionando Nome a tabela de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130825200134) do
+ActiveRecord::Schema.define(version: 20130828223447) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130825200134) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
