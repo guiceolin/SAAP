87d8a5a18d712a338c2ea8322a5d70c33c4d68b0
Guilherme Ceolin <guiceolin@gmail.com>
2013-08-28 00:03:55 -0300

    Criando model de professores com a rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 SAAP::Application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
+
+  resources :professors do
+    get :import, on: :collection
+    post :upload, on: :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
