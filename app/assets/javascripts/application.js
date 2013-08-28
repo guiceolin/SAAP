1555782d2e500e8f96fd44d970d32a4b0b24dd3a
Guilherme Ceolin <guiceolin@gmail.com>
2013-08-28 00:03:05 -0300

    Removendo Turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery-1.10.2.min.js
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 //= require jquery-migrate-1.2.1.min.js
 //= require jquery-ui-1.10.3.custom.min.js
 //= require jquery.ui.touch-punch.js
