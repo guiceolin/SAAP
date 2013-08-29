5853d3484a1b5c209b235aca4a06b324a6b17cf3
Guilherme Ceolin <guiceolin@gmail.com>
2013-08-28 23:18:22 -0300

    Adicionando coluna type ao usuário

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130828223447) do
+ActiveRecord::Schema.define(version: 20130829021149) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130828223447) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "type"
   end
 
 end
