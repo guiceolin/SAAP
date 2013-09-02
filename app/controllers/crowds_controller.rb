545a0ed629cf213c74a1f2f4958028ec862152a8
Guilherme Ceolin <guiceolin@gmail.com>
2013-09-01 21:02:43 -0300

    Crud de turmas

--- app/controllers/crowds_controller.rb
+++ app/controllers/crowds_controller.rb
@@ -4,4 +4,36 @@ class CrowdsController < ApplicationController
     @q = Crowd.search(params[:q])
     respond_with(@crowds = @q.result(distinct: true))
   end
+
+  def new
+    respond_with(@crowd = Crowd.new)
+  end
+
+  def create
+    @crowd = Crowd.new(crowd_params)
+    @crowd.save
+    respond_with(@crowd)
+  end
+
+  def edit
+    respond_with(@crowd = Crowd.find(params[:id]))
+  end
+
+  def update
+    @crowd = Crowd.find(params[:id])
+    @crowd.update_attributes(crowd_params)
+    respond_with(@crowd)
+  end
+
+  def destroy
+    @crowd = Crowd.find(params[:id])
+    @crowd.destroy
+    respond_with(@crowd)
+  end
+
+  private
+
+  def crowd_params
+    params[:crowd].permit(:code, :name, :year, :semester, :professor_id, :subject_id)
+  end
 end
--- app/models/crowd.rb
+++ app/models/crowd.rb
@@ -1,3 +1,7 @@
 class Crowd < ActiveRecord::Base
+  belongs_to :professor
+  belongs_to :subject
+  validates :code, :name, :presence => true, uniqueness: { scope: [:subject, :year, :semester] }
+  validates :year, :semester, :professor, :subject, :presence => true
 
 end
