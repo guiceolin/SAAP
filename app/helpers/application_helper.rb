978e4b45ea49f4d5e43197cf146a838f42ef4b99
Guilherme Ceolin <guiceolin@gmail.com>
2013-09-03 03:25:17 -0300

    Utilizando gravatar para imagem do perfil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def magic_form_for(model, attributes)
     render 'shared/magic_form_for', model: model, attributes: attributes
   end
+
+  def gravatar_url_for(user)
+    md5 = Digest::MD5.hexdigest(current_user.email.strip.downcase)
+    "http://www.gravatar.com/avatar/#{md5}"
+  end
 end
