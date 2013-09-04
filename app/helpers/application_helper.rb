c565e4f6e3ae58a355a5bd223d5b68c593a05d4a
Guilherme Ceolin <guiceolin@gmail.com>
2013-09-03 21:43:08 -0300

    usando Avatar padrão do gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
 
   def gravatar_url_for(user)
     md5 = Digest::MD5.hexdigest(current_user.email.strip.downcase)
-    "http://www.gravatar.com/avatar/#{md5}"
+    "http://www.gravatar.com/avatar/#{md5}?d=mm"
   end
 end
