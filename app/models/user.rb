8b771f07cb2a0ac12b7fe52a48a74eb1c117243f
Guilherme Ceolin <guiceolin@gmail.com>
2013-09-15 23:00:19 -0300

    relacionamento entre usuário e mensagem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   include Importable
 
+  has_many :deliveries, class_name: 'Messages::Delivery', foreign_key: 'recipient_id'
+  has_many :messages, through: :deliveries, class_name: 'Messages::Message'
+
   validates :name, presence: true
   validates :username, :email, presence: true, uniqueness: true
 
