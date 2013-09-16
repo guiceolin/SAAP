220f9994383e535fcfccbfa5c5e7a889c2686cf2
Guilherme Ceolin <guiceolin@gmail.com>
2013-09-15 23:08:59 -0300

    Relacao entre usuário e topicos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :deliveries, class_name: 'Messages::Delivery', foreign_key: 'recipient_id'
   has_many :messages, through: :deliveries, class_name: 'Messages::Message'
+  has_many :topics, through: :messages, class_name: 'Messages::Topic'
 
   validates :name, presence: true
   validates :username, :email, presence: true, uniqueness: true
