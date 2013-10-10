b2d908c2dc9be0d5ff68348d8a9d861a16b68f61
Guilherme Ceolin <guiceolin@gmail.com>
2013-10-09 21:23:40 -0300

    Ajustando as datas de criacao nos testes de topicos de estudantes

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -18,9 +18,9 @@ class StudentTest < ActiveSupport::TestCase
     crowd = Fabricate :crowd
     Fabricate :enrollment, crowd: crowd, student: student
     crowd_topics = []
-    crowd_topics_num = 3.times { crowd_topics << Fabricate(:topic, circle: crowd, creator: crowd.professor) }
+    crowd_topics_num = 3.times { Delorean.jump 30; crowd_topics << Fabricate(:topic, circle: crowd, creator: crowd.professor) }
     unapproved_topics = []
-    unapproved_topics_count = 2.times { unapproved_topics << Fabricate(:topic, circle: crowd, creator: student) }
+    unapproved_topics_count = 2.times { Delorean.jump 30; unapproved_topics << Fabricate(:topic, circle: crowd, creator: student) }
     assert student.topics.count == crowd_topics_num + unapproved_topics_count, "count: #{student.topics.count}"
     assert student.topics == (crowd_topics + unapproved_topics).sort_by(&:updated_at).reverse!
   end
