e091f57116d500975bd2f0ff6c5f7d997f7e9249
Guilherme Ceolin <guiceolin@gmail.com>
2013-10-09 21:22:01 -0300

    Ajustando o tamanho da descricao no fabricator de enunciado

--- test/fabricators/enunciation_fabricator.rb
+++ test/fabricators/enunciation_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:enunciation) do
   name        { Faker::Lorem.sentence }
-  description { Faker::Lorem.paragraphs }
+  description { Faker::Lorem.paragraph }
   end_at      { 10.days.from_now }
   crowd
 end
