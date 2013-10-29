960796a047189abfeb60add7868d59b53a2cdf6c
Guilherme Ceolin <guiceolin@gmail.com>
2013-10-29 00:20:59 -0200

    Alterando nome das chaves publicas para: username@'key.name'.pub

--- app/models/group.rb
+++ app/models/group.rb
@@ -19,7 +19,11 @@ class Group < ActiveRecord::Base
   end
 
   def students_key_names
-    students.map(&:pub_key_names).flatten
+    students.map { |s| { s => s.pub_keys } }.map do |hash|
+      hash.values[0].map do |pub_key|
+        "#{hash.keys[0].username}@#{pub_key.name}.pub"
+      end
+    end.flatten
   end
 
   def recipients
--- app/models/pub_key.rb
+++ app/models/pub_key.rb
@@ -1,4 +1,6 @@
 class PubKey < ActiveRecord::Base
   validate :name, :value, presence: true, uniqueness: :true
   belongs_to :user
+
+  delegate :username, to: :user
 end
