88717212239f795d2f310c9b4a07f183d90f036b
Guilherme Ceolin <guiceolin@gmail.com>
2013-10-29 01:44:33 -0200

    Atualizando os repos ao add/rm chave

--- app/models/pub_key.rb
+++ app/models/pub_key.rb
@@ -5,10 +5,16 @@ class PubKey < ActiveRecord::Base
 
   delegate :username, to: :student
 
-  after_create :add_key_to_gitosis
-  after_destroy :rm_key_from_gitosis
+  after_create :add_key_to_gitosis, :update_repos
+  after_destroy :rm_key_from_gitosis, :update_repos
 
   private
+
+  def update_repos
+    repositories.map(&:id).each do |id|
+      UpdateRepoWorker.perform_async(id)
+    end
+  end
 
   def rm_key_from_gitosis
     RmKeyWorker.perform_async(value, username, name)
