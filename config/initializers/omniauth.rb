f3d57b94d81e75c9b9b668292a4c52132f8b3ea6
Guilherme Ceolin <guiceolin@gmail.com>
2013-10-29 15:00:01 -0200

    Ajustando configuracoes do omniauth para sempre pedir permissao

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
-OmniAuth.config.full_host = "http://localhost:3000"
+OmniAuth.config.full_host = "http://localhost:3030"
 raise "omniauth config file not found!" unless File.exists?(Rails.root + "config/omniauth.yml")
 credentials = YAML.load(File.open(Rails.root + "config/omniauth.yml"))
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, credentials['id'], credentials['secret'], :scope => 'userinfo.email,userinfo.profile,calendar'
+  provider :google_oauth2, credentials['id'], credentials['secret'], :scope => 'userinfo.email,userinfo.profile,calendar',  :prompt => "consent"
 end
