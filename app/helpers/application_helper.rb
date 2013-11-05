376e4f569599909ce1ba4f68b931d18ba4eca0a9
Guilherme Ceolin <guiceolin@gmail.com>
2013-11-05 02:02:18 -0200

    Metodo para montar o breadcrumb do repo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
     md5 = Digest::MD5.hexdigest(current_user.email.strip.downcase)
     "http://www.gravatar.com/avatar/#{md5}?d=mm"
   end
+
+  def repo_breadcrumb_for(group, tree, path)
+    path ||= ""
+    out = []
+    out << link_to(group.name, group)
+    out << link_to(tree, tree_group_path(group.id, tree: tree ))
+    path.split('/').inject('') do |partial_path, part|
+      partial_path = File.join(partial_path, part)
+      out << link_to(part, tree_file_group_path(group, tree: tree, path: partial_path))
+      partial_path
+    end
+    out.join(' / ').html_safe
+  end
 end
