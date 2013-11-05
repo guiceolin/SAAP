22761335e158f6135f128c2a789ac7c623bb575b
Guilherme Ceolin <guiceolin@gmail.com>
2013-11-05 19:38:12 -0200

    separando dashboard de professor e alunos

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,21 @@
 class DashboardsController < ApplicationController
   def show
+    if current_user.is_a? Professor
+      render_professor
+    else
+      render_student
+    end
+  end
+
+  private
+
+  def render_professor
     @crowds = current_user.crowds
+    render :professor
+  end
+
+  def render_student
+    @crowds_with_groups = current_user.groups.group_by(&:crowd)
+    render :student
   end
 end
