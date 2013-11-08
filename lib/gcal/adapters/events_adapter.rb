ecbeb0ef02b393cc4d999954e4be1163268cc69d
Guilherme Ceolin <guiceolin@gmail.com>
2013-11-08 00:05:17 -0200

    Metodo no client para criar eventos

--- lib/gcal/adapters/events_adapter.rb
+++ lib/gcal/adapters/events_adapter.rb
@@ -14,6 +14,16 @@ module Gcal
           memo << Gcal::Event.new(event_hash, self)
         end
       end
+
+      def insert_event(calendar_id, attributes)
+        result = execute(:api_method => service.events.insert,
+                                :parameters => {'calendarId' => calendar_id},
+                                :body => JSON.dump(attributes),
+                                :headers => {'Content-Type' => 'application/json'})
+        event_hash = result.data.to_hash.merge!(calendar_id: calendar_id).symbolize_keys
+        Gcal::Event.new(event_hash, self)
+
+      end
     end
   end
 end
