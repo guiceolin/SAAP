4b3450a14e9575b807041e118077f2922484f5b6
Guilherme Ceolin <guiceolin@gmail.com>
2013-11-09 01:07:24 -0200

    Metodos para start/stop e status das tarefas

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,49 @@ class Task < ActiveRecord::Base
   has_many :children, foreign_key: :parent_id, class_name: 'Task'
 
   validates :scheduled_start_date, :scheduled_end_date, :description, presence: true
+
+  def start
+    if self.start_date.blank?
+      self.start_date = Date.today
+      self.save!
+    end
+  end
+
+  def started?
+    start_date.present? && !completed?
+  end
+
+  def complete
+    if self.start_date.present?
+      self.end_date = Date.today
+      self.save!
+    end
+  end
+
+  def completed?
+    end_date.present?
+  end
+
+  def status
+    if completed?
+      :low
+    elsif started?
+      :medium
+    else
+      :high
+    end
+  end
+
+  def real_start_date
+    start_date || scheduled_start_date
+  end
+
+  def real_end_date
+    end_date || scheduled_end_date
+  end
+
+  def to_s
+    description
+  end
+
 end
