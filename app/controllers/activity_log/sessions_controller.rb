55adf4c6b7be04b6d9556f41d31b5e888f4f5228
Guilherme Ceolin <guiceolin@gmail.com>
2013-11-11 02:52:05 -0200

    Retornando uma lista em branco no primeiro acesso ao log de sessao

--- app/controllers/activity_log/sessions_controller.rb
+++ app/controllers/activity_log/sessions_controller.rb
@@ -1,7 +1,12 @@
 class ActivityLog::SessionsController < ApplicationController
   def index
     authorize! :show, ActivityLog
-    @q =  ActivityLog.where(action: %w(login logout)).search(params[:q])
-    @activities = @q.result(distinct: true)
+    if params[:q]
+      @q =  ActivityLog.where(action: %w(login logout)).search(params[:q])
+      @activities = @q.result(distinct: true)
+    else
+      @q = ActivityLog.search({})
+      @activities = []
+    end
   end
 end
