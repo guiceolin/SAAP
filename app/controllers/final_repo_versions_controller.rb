192f212a6bf9ce1bb95728e2417310f967d2370a
Guilherme Ceolin <guiceolin@gmail.com>
2013-12-03 16:05:44 -0200

    Removendo commit

--- app/controllers/final_repo_versions_controller.rb
+++ app/controllers/final_repo_versions_controller.rb
@@ -2,7 +2,6 @@ class FinalRepoVersionsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     enunciation = @group.enunciation
-    binding.pry
     if enunciation.end_at < Date.today
       if  enunciation.accepts_after_deadline
         LateRepoVersion.create!(group: @group, creator: current_user)
